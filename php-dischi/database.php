399e4b500fb82b4550c8592e6f11dca691100e2b
Manuel della Gala <manuel.dellagala@gmail.com>
2021-05-20 13:09:10 +0200

    ok

---
+++ php-dischi/database.php
@@ -0,0 +1,48 @@
+<?php
+
+    $database = [
+        [
+            'title' => 'New Jersey',
+            'author' => 'Bon Jovi',
+            'year' => 1988,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg',
+            'genre' => 'Rock'
+        ],
+        [
+            'title' => 'Live at Wembley 86',
+            'author' => 'Queen',
+            'year' => 1992,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg',
+            'genre' => 'Pop'
+        ],
+        [
+            'title' => 'Ten\'s Summoner\'s Tales',
+            'author' => 'Sting',
+            'year' => 1993,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg',
+            'genre' => 'Pop'
+        ],
+        [
+            'title' => 'Steve Gadd band',
+            'author' => 'Steve Gadd Band',
+            'year' => 2018,
+            'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg',
+            'genre' => 'Jazz'
+        ],
+        [
+            'title' => 'And Justice for All',
+            'author' => 'Metallica',
+            'year' => 1988,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg',
+            'genre' => 'Metal'
+        ],
+        [
+            'title' => 'One more car, one more rider',
+            'author' => 'Eric Clapton',
+            'year' => 2002,
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg',
+            'genre' => 'Rock'
+        ]
+    ];
+
+?>
