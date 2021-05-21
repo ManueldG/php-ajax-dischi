a91784acf037871b47b7f03802b3d0c449f39d12
Manuel della Gala <manuel.dellagala@gmail.com>
2021-05-21 18:13:58 +0200

    ok

--- PHP/API/index.php
+++ PHP/API/index.php
@@ -5,12 +5,12 @@
 cors(); // abilito i permessi
 
 // i
-
+var_dump($_GET);
 
 for($i=0 ; $i<count($database) ; $i++)  
 var_dump( array_filter($database[$i], function($val,$k){
     //var_dump($k);
-    return ($k === 'title');
+    return ($k === 'pop');
 },ARRAY_FILTER_USE_BOTH) );
 
 
