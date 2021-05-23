6716279c9040062b9ca6616c45e746c300e7af03
Manuel della Gala <74384683+ManueldG@users.noreply.github.com>
2021-05-24 00:13:43 +0200

    Update index.php

--- PHP/API/index.php
+++ PHP/API/index.php
@@ -8,7 +8,7 @@ cors(); // abilito i permessi
 $db = array_filter($database, function($val){    
 
     $el=array_filter($val, function($value,$key){ //filtro i valori 
-        return (($_GET[$key] == $value));
+        return (($_GET[$key] == $value)); // stristr($value,$_GET[$key])===FALSE
     },ARRAY_FILTER_USE_BOTH);
 
     return (count($el)===count($_GET));  // filtro i valori con le stesse occorrenze
