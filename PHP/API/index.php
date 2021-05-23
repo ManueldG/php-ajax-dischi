93ebf0980860c8af7e1b5fcb39889f0295d772c0
Manuel della Gala <manuel.dellagala@gmail.com>
2021-05-23 23:43:21 +0200

    ok

--- PHP/API/index.php
+++ PHP/API/index.php
@@ -8,8 +8,12 @@ cors(); // abilito i permessi
 $db = array_filter($database, function($val){    
 
     $el=array_filter($val, function($value,$key){ //filtro i valori 
+        
+        if ($_GET[$key]===''){
+            unset($_GET[$key]);              
+        }
         return (($_GET[$key] == $value));
-    },ARRAY_FILTER_USE_BOTH);
+    },ARRAY_FILTER_USE_BOTH);    
 
     return (count($el)===count($_GET));  // filtro i valori con le stesse occorrenze
 },ARRAY_FILTER_USE_BOTH);
